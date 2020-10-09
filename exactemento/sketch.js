a4090a0094c097f4ff7d09b8cb15248a79ed8cc2
tieshoenselaar <tieshoenselaar@gmail.com>
2020-10-09 19:53:17 +0200

    test to check if the website works

--- exactemento/sketch.js
+++ exactemento/sketch.js
@@ -1,5 +1,17 @@
+let img;
+
+function preload() {
+    img = loadImage('img/waterpas.jpg');
+}
+
 function setup() {
-  // put setup code here
+    createCanvas(1500, 400);
+    image(img, 0, 0, 1500, 139);
+    input = createInput();
+    input.position(0, 300);
+
+    weight_question = createElement('h2', 'Hoeveel weegt het flesje inclusief inhoud?');
+    weight_question.position(0, 250);
 }
 
 function draw() {
